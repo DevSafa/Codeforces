0b79e7e94e4201c8ab5124883f5b0e300df6628d
Safa Barka <sbarka@e1r12p12.1337.ma>
2021-11-05 16:11:26 +0100

    fix condition of length

--- way-too-long-words.cpp
+++ way-too-long-words.cpp
@@ -13,16 +13,16 @@ int main()
             std::cin >> words[i];
         for(int i = 0 ; i < nbr_words ; i++)
         {
-            if(words[i].length() >= 10)
+            if(words[i].length() > 10)
             {
                 std::cout << words[i][0];
                 std::cout << words[i].length() - 2 ;
-                std::cout << words[i][words[i].length() - 1];
+                std::cout << words[i][words[i].length()-1];
                 std::cout << '\n';
             }
             else
                 std::cout << words[i] << '\n';
         }
     }else
-        std::cout << "failed" << std::endl;
+        std::cout << "failed" << std::endl; //not an int or press ctrl-D
 }
