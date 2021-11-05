4624d22fae2ed8ef81ef93927b66f8d91740f4d4
Safa Barka <sbarka@e1r12p12.1337.ma>
2021-11-05 19:40:07 +0100

    remove the arry structure

--- team.cpp
+++ team.cpp
@@ -6,18 +6,15 @@ int main()
 
    std::cin >> nbr_problems;
 
-   int sure[3 * nbr_problems];
-
    int count = 0;
    int submit = 0;
-
+   int number ;
    if(!std::cin.fail())
    {
-       for(int i = 0 ; i < nbr_problems * 3 ; i++)
-           std::cin >> sure[i];
         for(int i = 1 ; i <= nbr_problems * 3 ; i++)
         {
-            if(sure[i - 1] == 1 )
+            std::cin >> number;
+            if(number == 1 )
                 count++; 
             if(i % 3 == 0)
             {
