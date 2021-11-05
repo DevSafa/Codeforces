fda4aef391e6c67ff2609bad6b7b53c07e49811a
Safa Barka <sbarka@e1r12p12.1337.ma>
2021-11-05 16:03:26 +0100

    codeforces : Way Too Long Words

---
+++ way-too-long-words.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main()
+{
+    int nbr_words;
+
+    std::cin >> nbr_words;
+
+    if(!std::cin.fail())
+    {
+        std::string words[nbr_words] ;
+        for(int i = 0 ; i < nbr_words  ; i++)
+            std::cin >> words[i];
+        for(int i = 0 ; i < nbr_words ; i++)
+        {
+            if(words[i].length() >= 10)
+            {
+                std::cout << words[i][0];
+                std::cout << words[i].length() - 2 ;
+                std::cout << words[i][words[i].length() - 1];
+                std::cout << '\n';
+            }
+            else
+                std::cout << words[i] << '\n';
+        }
+    }else
+        std::cout << "failed" << std::endl;
+}
