44f02683790d3f6a5ff16eabe149f5ef9e34bf0e
sbarkadev <sbarka.dev@gmail.com>
2021-12-04 13:43:16 +0100

    next-round algo is correct

--- next-round.cpp
+++ next-round.cpp
@@ -1,35 +1,40 @@
 #include <iostream>
-
+ 
 int main()
 {
     int pos;
     int nbr;
-
+ 
     std::cin >> nbr;
-
+ 
     std::cin >> pos;
-
+ 
     int total = pos;
-
+ 
     int to_compare;
+   
     for(int i = 0 ; i < nbr ; i++)
     {
         int num;
         std::cin >> num;
-        if(i == 4)
-            to_compare = num;
-        if(i > 4)
+  
+        if(i < pos - 1)
         {
-            if(num >= to_compare)
-                total +=1;
+            if( num == 0)
+                 total--;
         }
+        else if(i == pos - 1)
+        {
+             if( num == 0)
+                total--;
+            to_compare = num;
+        }
+        else if ( i > pos -1)
+            if(num >= to_compare && num != 0)
+                total++;
     }
-
+ 
     std::cout << total << std::endl;
-
-   
-
-
-   
-
+ 
+ 
 }
